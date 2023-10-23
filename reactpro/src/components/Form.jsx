f9305fb613aaef14fe4c5ca295666d188d85b539
saba syed <sabaa9018@gmail.com>
2023-10-23 11:39:43 +0530

    javascript spread operator

--- reactpro/src/components/Form.jsx
+++ reactpro/src/components/Form.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 
 export default function Form(){
-  const [name,setName] = useState("");
-  function handleChange(e){
-    setName(e.target.value);
-  }
+  const [name,setName] = useState({firstName:"",lastName:""});
+  // function handleChange(e){
+  //   setName(e.target.value);
+  // }
   return (
   <div>
+    {name.firstName}-{name.lastName}
     <form >
       <input 
       //  onChange={function demo(e){
       //   return handleChange(e);
       // }} 
       // onChange={(e)=>handleChange(e)}
-      onChange={(e)=> setName(e.target.value)}
-      type="text" value={name}/>
+      onChange={(e)=> setName({...name,firstName:e.target.value})}
+      type="text" value={name.firstName}/>
+      <input type="text" onChange={(e)=>setName({...name,lastName:e.target.value})}value={name.lastName}/>
     </form>
   </div>
   );
